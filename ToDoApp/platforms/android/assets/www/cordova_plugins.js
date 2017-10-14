1b99be4e607e6b78a44b7654f21ac32f5a7a9487
Mohamad Abras <mohamad.abras@gmail.com>
2017-10-14 16:00:49 +0300

    add finale project files

---
+++ ToDoApp/platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,28 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "id": "cordova-plugin-splashscreen.SplashScreen",
+        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+        "pluginId": "cordova-plugin-splashscreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    },
+    {
+        "id": "cordova-sqlite-storage.SQLitePlugin",
+        "file": "plugins/cordova-sqlite-storage/www/SQLitePlugin.js",
+        "pluginId": "cordova-sqlite-storage",
+        "clobbers": [
+            "SQLitePlugin"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.3.2",
+    "cordova-plugin-splashscreen": "4.0.3",
+    "cordova-sqlite-storage": "2.0.4"
+};
+// BOTTOM OF METADATA
+});
---
+++ ToDoApp/platforms/android/assets/www/js/test.js
@@ -0,0 +1,12 @@
+//دالة لجلب معلومات الطالب
+/*
+
+
+*/
+function student() {
+    var abc;
+    var student_id;
+    var StudentId;
+    var studentId = [1, 2, 3];
+    1 + 1;
+}
